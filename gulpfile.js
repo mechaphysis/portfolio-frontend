9c8a6ca81a02ebcaa5df3a6eeded1c4c936e50de
mechaphysis <triptofante@gmail.com>
2018-09-12 13:20:13 +0200

    feat: Add deploy task for deploying to gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,20 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
-
+var deploy      = require('gulp-gh-pages');
 
 gulp.task('serve', ['sass'], function() {
   browserSync.init({
     server: "./"
   });
-
+  //Watch for changes in scss files and trigger sass task
   gulp.watch('sass/**/*.scss', ['sass']);
   gulp.watch('*.html').on('change', browserSync.reload);
 });
 
+/* This task will compile .scss to css and afterwards
+ * stream changes to browserSync
+ */
 gulp.task('sass', function() {
   gulp.src('sass/**/*.scss')
       .pipe(sass().on('error', sass.logError))
@@ -19,4 +22,12 @@ gulp.task('sass', function() {
       .pipe(browserSync.stream());
 });
 
+//Default gulp task will run serve task which itself will trigger sass task
 gulp.task('default', ['serve']);
+
+
+//Task for deploying to GitHub Pages
+gulp.task('deploy', function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
+});
