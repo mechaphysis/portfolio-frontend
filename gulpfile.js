cd5660ede6816f477883cf35f160826db756b2e7
mechaphysis <triptofante@gmail.com>
2018-09-14 16:54:41 +0200

    feat: Add dist files

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,49 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
-var deploy      = require('gulp-gh-pages');
+var ghPages = require('gulp-gh-pages');
 
 gulp.task('serve', ['sass'], function() {
-  browserSync.init({
-    server: "./"
-  });
   //Watch for changes in scss files and trigger sass task
   gulp.watch('sass/**/*.scss', ['sass']);
-  gulp.watch('*.html').on('change', browserSync.reload);
+  gulp.watch('./*.html', ['html-to-dist']);
+  gulp.watch('img/**/*', ['img-to-dist']);
+  gulp.watch('js/*', ['js-to-dist']);
+  browserSync.init({
+    server: "./dist"
+  });
 });
 
-/* This task will compile .scss to css and afterwards
+/* This task will compile .scss to css in dist and afterwards
  * stream changes to browserSync
  */
 gulp.task('sass', function() {
   gulp.src('sass/**/*.scss')
       .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('./css'))
+      .pipe(gulp.dest('dist/css'))
       .pipe(browserSync.stream());
+});
+/* The below tasks will copy html, img assets and js files
+ * (if present) to dist folder
+ */
+gulp.task('html-to-dist', function() {
+	gulp.src('./index.html')
+		.pipe(gulp.dest('./dist'))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('img-to-dist', function() {
+	gulp.src('./img/**/*')
+		.pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('js-to-dist', function() {
+	gulp.src(['js/*'])
+		.pipe(babel({
+			presets: ['env']
+			}))
+		.pipe(gulp.dest('dist/js'))
+		.pipe(browserSync.stream());
 });
 
 //Default gulp task will run serve task which itself will trigger sass task
@@ -29,5 +53,5 @@ gulp.task('default', ['serve']);
 //Task for deploying to GitHub Pages
 gulp.task('deploy', function () {
   return gulp.src("./dist/**/*")
-    .pipe(deploy())
+    .pipe(ghPages())
 });
